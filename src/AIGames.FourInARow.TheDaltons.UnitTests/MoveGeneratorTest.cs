e46b0a85f2edc87db7ddb4479180fd8890cd3d5b
Corniel <mail@corniel.nl>
2015-11-09 00:44:21 +0100

    Added move generator tests.

--- src/AIGames.FourInARow.TheDaltons.UnitTests/MoveGeneratorTest.cs
+++ src/AIGames.FourInARow.TheDaltons.UnitTests/MoveGeneratorTest.cs
@@ -6,7 +6,7 @@ namespace AIGames.FourInARow.TheDaltons.UnitTests
 	public class MoveGeneratorTest
 	{
 		[Test]
-		public void Test()
+		public void GetMoves_Column2Full_6NoneEmptyFields()
 		{
 			var generator = new MoveGenerator();
 			var field = Field.Parse(@"
@@ -76,6 +76,77 @@ namespace AIGames.FourInARow.TheDaltons.UnitTests
 				0,0,0,0,2,0,0;
 				1,0,0,0,1,0,0;
 				1,0,0,1,2,0,0"),
+			};
+
+			CollectionAssert.AreEqual(exp, act);
+
+		}
+
+		[Test]
+		public void GetMoves_Column3And3Full_5NoneEmptyFields()
+		{
+			var generator = new MoveGenerator();
+			var field = Field.Parse(@"
+				0,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0");
+
+			var act = generator.GetMoves(field, false);
+			var exp = new Field[]
+			{
+				// Column 0
+				Field.Parse(@"
+				0,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,2"),
+
+				// Column 1
+				Field.Parse(@"
+				0,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,2,0"),
+
+				// Column 2
+				Field.Empty,
+
+				// Column 3
+				Field.Empty,
+
+				// Column 4
+				Field.Parse(@"
+				0,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,2,1,2,0,0"),
+
+				// Column 5
+				Field.Parse(@"
+				0,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,2,0,1,2,0,0"),
+				
+				// Column 6
+				Field.Parse(@"
+				2,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0;
+				1,0,0,1,1,0,0;
+				2,0,0,2,1,0,0;
+				2,0,0,1,2,0,0"),
 			};
 
 			CollectionAssert.AreEqual(exp, act);
