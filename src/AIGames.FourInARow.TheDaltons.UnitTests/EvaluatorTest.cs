1d7b28b1cf345c3b4062512d8ee600e85742a610
Corniel <mail@corniel.nl>
2015-11-29 23:52:14 +0100

    Added detaction for double theatning.

---
+++ src/AIGames.FourInARow.TheDaltons.UnitTests/EvaluatorTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGames.FourInARow.TheDaltons.UnitTests
+{
+	[TestFixture]
+	public class EvaluatorTest
+	{
+		[Test]
+		public void GetScore_FieldWithForcedRed_WinOn21()
+		{
+			var field = Field.Parse(@"
+				0,0,0,0,0,0,0
+				0,0,0,0,0,0,0
+				0,0,0,1,1,0,1
+				0,0,2,1,2,0,2
+				0,0,1,2,1,0,1
+				0,2,2,1,2,0,2");
+
+			var act = Evaluator.GetScore(field, 17);
+			var exp = Scores.RedWins(21);
+			Assert.AreEqual(Scores.GetFormatted(exp), Scores.GetFormatted(act));
+		}
+	}
+}
--- src/AIGames.FourInARow.TheDaltons/Evaluator.cs
+++ src/AIGames.FourInARow.TheDaltons/Evaluator.cs
@@ -3,6 +3,7 @@
 	/// <summary>Gets a score for the board.</summary>
 	public static class Evaluator
 	{
+		private const int NotSet = short.MaxValue;
 		private static readonly int[] Pow2 = GetPow2();
 
 		private static int[] GetPow2()
@@ -21,48 +22,151 @@
 			unchecked
 			{
 #endif
-				ulong red = field.GetRed();
-				ulong yel = field.GetYellow();
+			var redToMove = (ply & 1) == 1;
 
-				var redTri = 0;
-				var yelTri = 0;
+			ulong red = field.GetRed();
+			ulong yel = field.GetYellow();
 
-				for (var index = 0; index < 69; index++)
+			ulong threatRed = 0;
+			ulong threatYel = 0;
+
+			var redTri = 0;
+			var yelTri = 0;
+
+			var forcedRed = NotSet;
+			var forcedYel = NotSet;
+
+			for (var index = 0; index < 69; index++)
+			{
+				var mask = Field.Connect4[index];
+				var matchRed = red & mask;
+				if (matchRed == mask) { return Scores.RedWins(ply); }
+
+				var matchYel = yel & mask;
+				if (matchYel == mask) { return Scores.YelWins(ply); }
+
+				// Both have a match. Skip it.
+				if (matchRed != 0 && matchYel != 0) { continue; }
+
+				if (matchRed != 0)
 				{
-					var mask = Field.Connect4[index];
-					var matchRed = red & mask;
-					if (matchRed == mask) { return Scores.RedWins(ply); }
-
-					var matchYel = yel & mask;
-					if (matchYel == mask) { return Scores.YelWins(ply); }
-
-					// Both have a match. Skip it.
-					if (matchRed != 0 && matchYel != 0) { continue; }
-
-					if (matchRed != 0)
+					for (var i = 0; i < 4; i++)
 					{
-						for (var i = 0; i < 4; i++)
+						var lookup = (index << 2) | i;
+						if (Field.Connect3Out4[lookup] == matchRed)
 						{
-							if (Field.Connect3Out4[(index << 2) | i] == matchRed)
-							{
-								redTri++;
-								break;
-							}
-						}
-					}
-					else
-					{
-						for (var i = 0; i < 4; i++)
-						{
-							if (Field.Connect3Out4[(index << 2) | i] == matchYel)
-							{
-								yelTri++;
-								break;
-							}
+							redTri++;
+							threatRed |= Field.Connect4Threat[lookup];
+							break;
 						}
 					}
 				}
-				return Pow2[redTri] - Pow2[yelTri];
+				else
+				{
+					for (var i = 0; i < 4; i++)
+					{
+						var lookup = (index << 2) | i;
+						if (Field.Connect3Out4[lookup] == matchYel)
+						{
+							yelTri++;
+							threatYel |= Field.Connect4Threat[lookup];
+							break;
+						}
+					}
+				}
+			}
+
+			if (redTri > 0 || yelTri > 0)
+			{
+				var mixed = red | yel;
+
+				var leftRed = redTri;
+				var leftYel = yelTri;
+
+				for (ulong col = 0; col < 7; col++)
+				{
+					var colRed = threatRed & Field.ColumnMasks[col];
+					var colYel = threatYel & Field.ColumnMasks[col];
+
+					if (colRed != 0 || colYel != 0)
+					{
+						var colMix = mixed & Field.ColumnMasks[col];
+
+						var strR = Field.ToString(threatRed);
+						var strY = Field.ToString(threatYel);
+
+						var min = 0;
+						var row = 0;
+						var rowR = 0;
+						var rowY = 0;
+
+						for (ulong mask = col + 1; mask < Field.Mask; mask <<= 8)
+						{
+							row++;
+							if /**/ ((colMix & mask) != 0)
+							{
+								min = row;
+							}
+							else if ((colRed & mask) != 0)
+							{
+								if (rowR + 1 == row)
+								{
+									var forced = row - min;
+									if (rowY == 0 && forced < forcedRed)
+									{
+										forcedRed = forced;
+										break;
+									}
+								}
+								rowR = row;
+								leftRed--;
+							}
+							else if ((colYel & mask) != 0)
+							{
+								if (rowY + 1 == row)
+								{
+									var forced = row - min;
+									if (rowR == 0 && forced < forcedYel)
+									{
+										forcedYel = forced;
+										break;
+									}
+								}
+								rowR = row;
+								leftYel--;
+							}
+							if (leftRed == 0 && leftYel == 0) { break; }
+						}
+					}
+				}
+			}
+
+			if (forcedRed != NotSet || forcedYel != NotSet)
+			{
+				if (redToMove)
+				{
+					if (forcedRed <= forcedYel)
+					{
+						return Scores.RedWins(ply + forcedRed);
+					}
+					else
+					{
+						return Scores.YelWins(ply + forcedRed + 1);
+					}
+				}
+				else
+				{
+					if (forcedRed < forcedYel)
+					{
+						return Scores.RedWins(ply + forcedRed + 1);
+					}
+					else
+					{
+						return Scores.YelWins(ply + forcedRed);
+					}
+				}
+			}
+			return Pow2[redTri] - Pow2[yelTri];
 #if !DEBUG
 			}
 #endif
