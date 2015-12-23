283946f631e85ee6b176667069d5813a7437f558
Corniel <mail@corniel.nl>
2015-12-23 10:52:42 +0100

    Tweaked movegenerator.

--- src/AIGames.FourInARow.TheDaltons/MoveGenerator.cs
+++ src/AIGames.FourInARow.TheDaltons/MoveGenerator.cs
@@ -3,7 +3,7 @@
 	public class MoveGenerator
 	{
 		private const ulong RowMask = 0x010101010101;
-	
+
 		public Field[] GetMoves(Field field, bool IsRed)
 		{
 			var moves = new Field[7];
@@ -13,19 +13,20 @@
 			for (var col = 0; col < 7; col++)
 			{
 				var test = (occupied >> col) & RowMask;
-				var row = -1;
+				var row = 0;
 
 				switch (test)
 				{
-					case 0x0000000000: row = 0; break;
-					case 0x0000000001: row = 1; break;
-					case 0x0000000101: row = 2; break;
-					case 0x0000010101: row = 3; break;
-					case 0x0001010101: row = 4; break;
-					case 0x0101010101: row = 5; break;
+					case 0x000000000000: break;
+					case 0x000000000001: row = 1; break;
+					case 0x000000000101: row = 2; break;
+					case 0x000000010101: row = 3; break;
+					case 0x000001010101: row = 4; break;
+					case 0x000101010101: row = 5; break;
+					case 0x010101010101: row = 6; break;
 					default: break;
 				}
-				if (row != -1)
+				if (row != 6)
 				{
 					var move = 1UL << ((row << 3) | col);
 					moves[col] = IsRed ? field.MoveRed(move) : field.MoveYellow(move);
